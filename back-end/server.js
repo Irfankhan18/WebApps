8075c617a3f931b9e4777e553daa0fa281413f08
irfankhan18 <irfankhan@hotmail.ca>
2016-12-07 22:38:51 -0500

    working app

--- back-end/server.js
+++ back-end/server.js
@@ -4,20 +4,21 @@ var bodyParser = require('body-parser');
 var mongoose = require ('mongoose');
 
 
+var checkAuthenticated =require('./services/checkAuthenticated');
 var auth = require('./controllers/auth');
 var message = require('./controllers/message');
-
+var cors = require('./services/cors')
+//Middleware
 app.use(bodyParser.json());
+app.use(cors);
 
-app.use(function(req, res, next){
-   res.header("Access-Control-Allow-Origin","*");
-   res.header("Access-Control-Allow-Headers","Content-Type, Authorization");
-   next();
-})
+//Requests
 app.post('/auth/register', auth.register);
+app.post('/auth/login', auth.login);
 app.get('/api/message', message.get);
-app.post('/api/message', message.post);
+app.post('/api/message', checkAuthenticated, message.post);
 
+//connection
 mongoose.connect("mongodb://localhost:27017/test", function(err, db){
   if(!err){
     console.log("we are connected to mongo");
